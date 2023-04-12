a56c3a247e9ffcef23859f8b7807aba033bdfc19
liupeidong <liupeidong@sensorsdata.cn>
2023-04-12 19:43:22 +0800

    .

--- sort.js
+++ sort.js
@@ -13,12 +13,33 @@ const bubble = (arr) => {
     }
   }
   console.timeEnd('冒泡');
-  console.log(count);
 }
 
-/** 选择 */
+/** 插入 */
+const insert = (arr) => {
+  console.time('插入');
+  for (let i = 1; i < arr.length; i ++) {
+    const compare = arr[i];
+    let j = i - 1;
+    // arr[i] 左侧的元素都是已经排好序的
+    while (j >= 0 && arr[j] > compare) {
+      // 已排序的元素大于当前要插入元素, 则已排序的元素后移
+      arr[j + 1] = arr[j];
+      j --;
+    }
+    // 所有的后移完成后, 即 arr[j] < compare, 则把 compare 插入到 arr[j] 之后
+    arr[j + 1] = compare;
+  }
+  console.timeEnd('插入');
+}
 
+/** 快排 */
+const quick = (arr) => {
+  console.time('快排');
 
-const numbers = randomNumbers(1000);
+}
+
+const numbers = randomNumbers(100);
 // const numbers = [1,2,3,4,5,6,7,8,9]
 bubble(numbers.slice());
+insert(numbers.slice());
